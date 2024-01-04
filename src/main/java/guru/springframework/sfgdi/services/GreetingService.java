bcb735c98b7241b23b9be770ad5ae8f88044776c
Nico Reinsch <nico.reinsch@hotmail.de>
2024-01-04 19:00:51 +0100

    Initial commit

---
+++ src/main/java/guru/springframework/sfgdi/services/GreetingService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+public interface GreetingService {
+
+
+    String sayGreeting();
+
+}
